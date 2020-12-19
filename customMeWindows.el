290303b89e5274dc28f5ac35a84a4eb5f3bfad12
guillermoToshiba <memocampeon35@gmail.com>
2020-12-19 13:26:20 -0600

    He agregando todos los paquentes esenciales correctamente en un archivo org

--- customMeWindows.el
+++ customMeWindows.el
@@ -168,7 +168,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(helm yasnippet-snippets yasnippet use-package)))
+ '(package-selected-packages
+   '(quickrun treemacs company-quickhelp popwin which-key helm-descbinds multiple-cursors rainbow-delimiters highlight-numbers neotree company counsel ivy avy helm yasnippet-snippets yasnippet use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- dirCustomOrg/customMeWindowsORG.el
+++ dirCustomOrg/customMeWindowsORG.el
@@ -42,3 +42,96 @@
 (("C-s" . helm-occur))
 (([f10] . 'helm-semantic-or-imenu))
 (("C-c g" . helm-grep-do-git-grep)))  ;; Search using grep in a git project
+
+(use-package helm-descbinds
+:ensure t
+:bind ("C-h b" . helm-descbinds))
+
+(use-package which-key 
+:ensure t 
+:init
+(which-key-mode)
+)
+
+(use-package ivy
+  :ensure t  
+  )
+
+(use-package counsel
+  :ensure t  
+  :bind
+  (("C-x b" . counsel-switch-buffer))
+  (("C-x C-f" . counsel-find-file))
+  )
+
+(use-package avy
+  :ensure t
+  :bind  
+  (("C-c j" . avy-goto-word-or-subword-1))
+  (("C-:" . avy-goto-line)))
+
+(use-package popwin
+  :ensure t
+  :init
+  (popwin-mode 1)
+)
+
+(use-package company
+  :ensure t
+  :config
+  (setq company-idle-delay 0.2)
+  (setq company-show-numbers t)
+  (setq company-tooltip-limit 10)
+  (setq company-minimum-prefix-length 2)
+  (setq company-tooltip-align-annotations t)
+  ;; invert the navigation direction if the the completion popup-isearch-match
+  ;; is displayed on top (happens near the bottom of windows)
+  (setq company-tooltip-flip-when-above t)
+  (global-company-mode)
+  :bind
+  (("<C-return>" . company-complete))
+)
+
+(use-package company-quickhelp
+  :ensure t
+  :config
+  (company-quickhelp-mode)
+)
+
+(use-package highlight-numbers
+:ensure t
+:init
+(add-hook 'prog-mode-hook 'highlight-numbers-mode) 
+)
+
+(use-package neotree
+:ensure t
+:config
+(setq neo-theme 'arrow)
+(global-set-key [f9] 'neotree-toggle))
+
+(use-package treemacs
+  :ensure t
+  :config
+  (global-set-key [f12] 'treemacs)
+  (global-set-key (kbd "C-c p") 'treemacs-add-project)
+)
+
+(use-package rainbow-delimiters
+:ensure t
+)
+
+(use-package multiple-cursors
+:ensure t
+:bind
+(("C-c C-m" . mc/edit-lines))
+(("C->" . mc/mark-next-like-this))
+(("C-<" . mc/mark-previous-like-this))
+(("C-c C-<" . 'mc/mark-all-like-this))
+(("C-c C-a" . mc/skip-to-previous-like-this))
+(("C-M-<mouse-1>" . mc/add-cursor-on-click))
+)
+
+(use-package quickrun 
+:ensure t
+:bind ("C-c r" . quickrun))
