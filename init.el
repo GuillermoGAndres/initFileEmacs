c07f02dc69bfb82cc7e8b9cf130c8254e02557cf
GuillermoLenovo <gerardobec35@gmail.com>
2020-12-19 14:38:33 -0600

    Agregue pocas lineas  para que funcione tanto en windows como Linux el archivo de configuracion

--- init.el
+++ init.el
@@ -9,31 +9,36 @@
       ;Cargar package de un archivos org, compila los archivos org y lo convierte en .el, sera nuestro archivo principal
       (org-babel-load-file (expand-file-name "~/.emacs.d/initFileEmacs/dirCustomOrg/customMeWindowsORG.org"))
       
-      )  
-  (progn
-    ;(message "Estas es un system Linux!")
-    (setq custom-file "/home/guillermo/.emacs.d/emacs-configuration/customMe.el")
-    (load custom-file)
-
-    ;Cargar archivos org, compila los archivos org y lo convierte en .el, sera nuestro archivo principal
-    ;(org-babel-load-file (expand-file-name "~/.emacs.d/emacs-configuration/dirCustomOrg/customMeORG.org"))
-    
-    )  
+      )    
 )
 
+(if (eq system-type 'gnu/linux)
+    (progn
+      ;(message "Estas es un system Linux!")
+      (setq custom-file "/home/guillermo/.emacs.d/initFileEmacs/customMe.el")
+      (load custom-file)
+      ;Cargar archivos org, compila los archivos org y lo convierte en .el, sera nuestro archivo principal
+      ;(org-babel-load-file (expand-file-name "~/.emacs.d/emacs-configuration/dirCustomOrg/customMeORG.org"))
+    
+    )    
+)
 
+; Ahora es turno de cargar los temas
+;Pava ver menu bar solo cuando es ejecutado windos-system (GUI), valor x y si no hay nil
+(if window-system
+    (progn
+      (setq custom-file "~/.emacs.d/initFileEmacs/customApariencia.el")
+      (load custom-file)
+    )    
+)
+
+;Plugins 
 ;Cargando un plugin externo esta es una manera mas elegante de hacerlo para cargarlo.
 (add-to-list 'load-path "~/.emacs.d/initFileEmacs/pluginsUser")
 (load "resize-frame")
 
-; Ahora es turno de cargar los temas
-(setq custom-file "~/.emacs.d/initFileEmacs/customApariencia.el")
-(load custom-file)
 
 ; Archivo donde se guardaran todas las personalizaciones, es un archivo auxiliar para observar los atributos.
 (setq custom-file "~/.emacs.d/initFileEmacs/pluginsUser/customFacesMe.el")
 (load custom-file)
-
-
-
 
